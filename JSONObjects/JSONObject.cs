b69fed354d2aa567ff60c8c9a8629b1a93f145ce
Pawel Gojlik <gojlikus@wp.pl>
2019-04-11 11:11:12 +0200

    Read JSON from url, parse to list of objects, sort and display

---
+++ JSONObjects/JSONObject.cs
@@ -0,0 +1,10 @@
+﻿namespace JSONObjects
+{
+    public class JSONObject
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+        public double value { get; set; }
+        public bool registered { get; set; }
+    }
+}
--- JSONObjects/Program.cs
+++ JSONObjects/Program.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace JSONObjects
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            string json;
+
+            using (WebClient webClient = new WebClient())
+            {
+                json = webClient.DownloadString("https://api.jsonbin.io/b/5caefde5814711458b4021df/5");
+            }
+
+            var JSONObjectsList = JsonConvert.DeserializeObject<List<JSONObject>>(json).OrderBy(o=>o.value);
+
+            
+
+            foreach (var item in JSONObjectsList)
+            {
+                Console.WriteLine("id: {0} ,name: {1}, value: {2}, registered: {3}",
+                    item.id, item.name, item.value, item.registered);
+            }
         }
     }
 }
